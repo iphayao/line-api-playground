7a0520af5957dc38bc6b78029c09408ecde5db1a
iphayao <phayaob@gmail.com>
2019-01-19 09:53:09 +0700

    Initialized project

---
+++ src/main/java/com/iphayao/lineapiplayground/LineApiPlaygroundApplication.java
@@ -0,0 +1,14 @@
+package com.iphayao.lineapiplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineApiPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineApiPlaygroundApplication.class, args);
+	}
+
+}
+
